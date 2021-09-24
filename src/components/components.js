b9dd63b1cd8caab6a72e76e3b14d4c93afccd7f6
Justin Wyllie <justinwyllie@hotmail.co.uk>
2021-09-24 18:48:14 +0700

    Put components into separate file

---
+++ src/components/components.js
@@ -0,0 +1,75 @@
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import { Field, ErrorMessage } from 'formik';
+import { Button} from 'react-bootstrap';
+
+const Instruction = ({instruction, idx}) => {
+        
+    return (
+    <Row>
+        <Col md={2}>
+            <span className="badge badge-info">{instruction.lang}</span>
+        </Col>    
+        <Col md={10}>
+            <Field as="textarea"  className="kea-wide-field"
+                name={`instructions.${idx}.text`} rows={3}
+            >
+            </Field>
+            <ErrorMessage
+                          name={`instructions.${idx}.text`}
+                          component="div"
+                          className="field-error"
+            />
+        </Col>
+    </Row>)
+}
+
+const GapFillQuestion = ({idx, remove}) => {
+        
+    return (
+    <div className="kea-additional-field-block"> 
+        <Form.Group as={Row}>
+            <Form.Label column md={2}>{idx}. (question)</Form.Label>
+            <Col md={10}>
+                <Field className="kea-wide-field kea-additional-field" name={`questions.${idx}.question`}
+                    placeholder="Example ___ sentence with blank and (keyword)"
+                    type="text"
+                />
+               
+                <ErrorMessage
+                          name={`questions.${idx}.question`}
+                          component="div"
+                          className="field-error"
+                />
+            </Col>
+        </Form.Group> 
+        <Form.Group as={Row}>
+            <Form.Label column md={2}>{idx}. (answer)</Form.Label>
+            <Col md={10}>
+                <Field className="kea-wide-field kea-question-field" name={`questions.${idx}.answer`}
+                    placeholder="keywordAnswer"
+                    type="text"
+                />
+                <ErrorMessage
+                          name={`questions.${idx}.answer`}
+                          component="div"
+                          className="field-error"
+                />
+            </Col>
+        </Form.Group>
+        <Row>
+            <Col className="text-right">
+                <Button 
+                    type="button"
+                    className="secondary"
+                    onClick={() => remove(idx)}>-</Button>   
+            </Col>
+        </Row> 
+    </div>)
+}
+
+export {
+    Instruction,
+    GapFillQuestion
+}
--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import { useEntityProp } from '@wordpress/core-data';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import { Button} from 'react-bootstrap';
-import { Formik, Field, FieldArray, ErrorMessage } from 'formik'; 
+import { Formik, Field, FieldArray, ErrorMessage } from 'formik';
+import { Instruction, GapFillQuestion } from './components/components';
 //import bootstrap CSS 
 //rather lucily the webpack installed by wp-scripts includes a process for scss
 //and by default builds it to index.css minified. 
@@ -59,82 +59,6 @@ const additionalLangs = ['ru'];
 const supportedLangs = [defaultLang, ...additionalLangs];
 console.log("set to false");
 //{`instructions.${idx}.text`} 
-
-const Instruction = ({
-    instruction,
-    idx
-    }) => {
-        
-    return (
-    <Row>
-        <Col md={2}>
-            <span className="badge badge-info">{instruction.lang}</span>
-        </Col>    
-        <Col md={10}>
-            <Field as="textarea"  className="kea-wide-field"
-                name={`instructions.${idx}.text`} rows={3}
-            >
-            </Field>
-            <ErrorMessage
-                          name={`instructions.${idx}.text`}
-                          component="div"
-                          className="field-error"
-            />
-        </Col>
-    </Row>)
-}
-
-const GapFillQuestion = ({values,
-    idx,
-    errors,
-    touched,
-    handleChange,
-    handleBlur,
-    handleSubmit,
-    isSubmitting,
-    remove}) => {
-        
-    return (
-    <div className="kea-additional-field-block"> 
-        <Form.Group as={Row}>
-            <Form.Label column md={2}>{idx}. (question)</Form.Label>
-            <Col md={10}>
-                <Field className="kea-wide-field kea-additional-field" name={`questions.${idx}.question`}
-                    placeholder="Example ___ sentence with blank and (keyword)"
-                    type="text"
-                />
-               
-                <ErrorMessage
-                          name={`questions.${idx}.question`}
-                          component="div"
-                          className="field-error"
-                />
-            </Col>
-        </Form.Group> 
-        <Form.Group as={Row}>
-            <Form.Label column md={2}>{idx}. (answer)</Form.Label>
-            <Col md={10}>
-                <Field className="kea-wide-field kea-question-field" name={`questions.${idx}.answer`}
-                    placeholder="keywordAnswer"
-                    type="text"
-                />
-                <ErrorMessage
-                          name={`questions.${idx}.answer`}
-                          component="div"
-                          className="field-error"
-                />
-            </Col>
-        </Form.Group>
-        <Row>
-            <Col className="text-right">
-                <Button 
-                    type="button"
-                    className="secondary"
-                    onClick={() => remove(idx)}>-</Button>   
-            </Col>
-        </Row> 
-    </div>)
-}
 
 
 
