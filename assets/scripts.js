2b2d9200a309109cd4fa20e5cfcf1afb834d9a9a
Justin Wyllie <justinwyllie@hotmail.co.uk>
2024-03-03 20:53:55 +0300

    WIP - duplicating posts

---
+++ assets/scripts.js
@@ -0,0 +1,10 @@
+
+
+
+window.addEventListener("load", function() {
+
+    document.getElementById("kea-duplicate-activities").addEventListener("click", function() {
+        console.log("scripts2");
+
+    })
+})
